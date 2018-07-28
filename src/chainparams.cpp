deb6a4a5d4c60c66d3616501e11087a7bd673061
Bergaoui Bacem <bacem@arcturustech.com>
2018-07-28 09:48:24 +0100

    Drop reg test genesis debug message

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -585,7 +585,7 @@ public:
 
         genesis = CreateGenesisBlock(1417713337, 1096447, 0x207fffff, 1, 50 * COIN);
         consensus.hashGenesisBlock = genesis.GetHash();
-        std::cout << "regtest genesis is : " << genesis.GetHash().ToString() << " \n";
+      //  std::cout << "regtest genesis is : " << genesis.GetHash().ToString() << " \n";
 
       //  assert(consensus.hashGenesisBlock == uint256S("0x000008ca1832a4baf228eb1553c03d3a2c8e02399550dd6ea8d65cec3ef23d2e"));
       //  assert(genesis.hashMerkleRoot == uint256S("0xe0028eb9648db56b1ac77cf090b99048a8007e2bb64b68f092c03c7f56a662c7"));
